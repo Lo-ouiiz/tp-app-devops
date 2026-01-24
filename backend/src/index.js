e59a4f3a08aff4061d7fec8bee68000de00b8bab
Louise Mendiburu <louisemendiburu@gmail.com>
2026-01-24 20:49:04 +0100

    feat: add basic logger for backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -46,6 +46,26 @@ app.get('/metrics', async (req, res) => {
   res.end(await client.register.metrics());
 });
 
+// Logger HTTP basique pour Loki
+app.use((req, res, next) => {
+  const start = Date.now();
+
+  res.on("finish", () => {
+    const duration = Date.now() - start;
+
+    const level =
+      res.statusCode >= 500 ? "ERROR" :
+      res.statusCode >= 400 ? "WARN" :
+      "INFO";
+
+    console.log(
+      `[${level}] ${req.method} ${req.originalUrl} ${res.statusCode} ${duration}ms`
+    );
+  });
+
+  next();
+});
+
 // Routes API
 app.use('/api/users', userRoutes);
 app.use('/api/subscriptions', subscriptionRoutes);
